7fe04b79fc8a672004af224ab1cf4ce5468d7a42
Juan José Bedoya Isaza <juabedisa@alu.edu.gva.es>
2025-11-04 16:26:09 +0100

    Creación del modelo Receta en el models.py

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from pydantic import BaseModel
 from datetime import date
 import uuid
+
 class Usuario (BaseModel):
     id: str =str(uuid.uuid4())
     nombreUsuario: str
@@ -13,3 +14,15 @@ class Usuario (BaseModel):
     añoNacimiento: Optional[str]= None 
     fotoPerfil: Optional [str]=None #URL o nombre del archivo
     rol:str = "usuario"
+
+class Receta(BaseModel):
+    id: str = str(uuid.uuid4()) #Identificar Unico
+    titulo: str
+    descripcion: Optional[str] = None
+    tiempoPreparacion: Optional[int] = None #Lo introducido en números
+    porciones: Optional[int] = None
+    dificultad: Optional[str] = None  # "Fácil", "Media", "Difícil"
+    fechaCreacion: date = date.today()
+    idUsuario: uuid.UUID  # referencia al usuario que la creó
+    imagen: Optional[str] = None  # URL o nombre del archivo de imagen
+    categoria: Optional[str] = None  # Ej: "Postre", "Vegano", etc.
