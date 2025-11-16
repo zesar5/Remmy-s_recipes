35d75a890dfd93aa5690cc43dbd4b2abca168380
Juan José <juabedisa@alu.edu.gva.es>
2025-11-17 00:59:38 +0100

    Finalización de la tarea "Validar los datos recibidos antes de guardar."

--- Backend/app/schemas.py
+++ Backend/app/schemas.py
@@ -1,15 +1,21 @@
 from pydantic import BaseModel
 
 class RecetaBase(BaseModel):
-    titulo: str
-    descripcion: str
+    titulo: str = Field(..., min_length=3, max_length=50)
+    descripcion: str = Field(..., min_length=10, max_length=500)
+    tiempoPreparacion: Optional[int] = Field(None, ge=1, description="Minutos de preparación")
+    porciones: Optional[int] = Field(None, ge=1)
+    dificultad: Optional[str] = Field(None, pattern="^(Fácil|Media|Difícil)$")
+    categoria: Optional[str] = Field(None, min_length=3)
     imagen: Optional[str] = None
+    idUsuario: Optional[str] = None
 
 class RecetaCreate(RecetaBase):
     pass
 
 class Receta(RecetaBase):
     id: int
+    fechaCreacion: date
 
     class Config:
         orm_mode = True
