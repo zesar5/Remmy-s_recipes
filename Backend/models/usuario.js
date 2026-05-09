3601d1d9afd9f45052e61f628783ab596f9b69ef
Rubén <rubaroman@alu.edu.gva.es>
2026-05-09 16:34:12 +0200

    arreglo del inicio de sesión fallido

--- Backend/models/usuario.js
+++ Backend/models/usuario.js
@@ -70,7 +70,7 @@ class Usuario {
     const user = rows[0];
 
     if (user.imagen) {
-      user.fotoPerfil = user.imagen;
+      user.fotoPerfil = `/uploads/usuarios/${user.imagen}`;
     } else {
       user.fotoPerfil = null;
     }
